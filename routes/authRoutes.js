99613dc7f014e771b510b8a1a2ce9765e4d92bf2
Abhigyan <abhigyan170404@gmail.com>
2026-05-18 14:06:00 +0530

    fixing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -111,7 +111,12 @@ router.post("/login", async (req, res) => {
 
     res.json({
       token,
-      user,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
     });
   } catch (error) {
     res.status(500).json({
